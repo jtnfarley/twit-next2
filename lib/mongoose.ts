4de700070fb31e4056a0913423894362d143b070
John Farley <john@pcmatic.com>
2025-08-17 16:43:31 -0400

    fixed mongo env var

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -13,7 +13,7 @@ export const connectToDB = async (): Promise<void> => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URL as string)
+        await mongoose.connect(process.env.MONGODB_URI as string)
         isConnected = true
         console.log('MongoDB connected')
     } catch (error) {
