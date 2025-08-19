52bb89358ab689903bf933ca186a801b5306387e
John Farley <john@pcmatic.com>
2025-08-18 20:58:39 -0400

    fixed mongo uri issue

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,20 +1,4 @@
-import { currentUser } from "@clerk/nextjs/server";
-
-import { createUser } from '@/lib/actions/user.actions'
-
 export default async function Page() {
-
-	const user = await currentUser()
-
-	if (user) {
-		await createUser({
-			userId: user.id,
-			email: user.emailAddresses[0].emailAddress,
-			username: user.username || '',
-			name: `${user.firstName || ''} ${user.lastName || ''}`,
-			image: user.imageUrl || ''
-		})
-	}
 	
 	return (
 		<>
